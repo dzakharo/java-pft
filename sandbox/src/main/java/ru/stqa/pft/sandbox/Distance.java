3b0a533b473d61e16bb3feef42ae0d911aedc2d0
T-SYSTEMS\dzakharo <dmitrii.zakharov@t-systems.ru>
2019-02-08 09:48:13 +0300

    Задание 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+
+public class Distance {
+
+  public static void main(String[] args) {
+
+    Point p1=new Point(2,3);
+    Point p2=new Point(3,7);
+
+    System.out.println("Расстояние между точками ("+p1.x+","+p1.y+")"+" и ("+p2.x+","+p2.y+") равно "+p1.distance(p2));
+  }
+}
