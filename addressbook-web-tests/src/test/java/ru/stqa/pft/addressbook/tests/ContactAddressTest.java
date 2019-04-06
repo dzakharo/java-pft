2d0b502f60cab5135dfa46b7929c5b2aca7dbaf0
dzakharo <dmitrii.zakharov@t-systems.ru>
2019-04-06 21:39:48 +0300

    задание 11 - исправление

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTest.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.tests.TestBase;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddressTest extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().homePage();
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData()
+              .withFirstName("test").withLastName("test").withAddress("teees").withHome("111").withMobile("222").withWork("333").withEmail("email@email.com"));
+    }
+  }
+
+  @Test
+  public void testContactAddress() {
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+  }
+
+}
