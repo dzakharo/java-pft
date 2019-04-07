17d0da59c60a46feaa2f61db7cec924f95b2d11b
T-SYSTEMS\dzakharo <dmitrii.zakharov@t-systems.ru>
2019-04-07 19:06:41 +0300

    Задание 13 - Параметризованные тесты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,24 +1,47 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 
 public class ContactCreationTest extends TestBase{
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader =new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml="";
+    String line = reader.readLine();
+    while (line!=null){
+      xml+=line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> groups = (List<ContactData>) xstream.fromXML(xml);
+    return groups.stream().map((g)->new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
-  @Test
-  public void testContactCreation() {
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo =new File("src/test/resources/img.jpg");
-    ContactData contact = new ContactData()
-            .withFirstName("Name78").withLastName("Test5").withEmail("tet@tete.rr").withMobile("+5555").withPhoto(photo);
+  //  File photo =new File("src/test/resources/img.jpg");
     app.contact().create(contact);
     assertThat(app.contact().count(),equalTo(before.size()+1));
     Contacts after=app.contact().all();
